68d6d3859444860a925c2dc0324570b380365fe8
stevelee41 <stevelee41@gmail.com>
2020-09-16 21:42:19 -0400

    Add files via upload

--- hhub.js
+++ hhub.js
@@ -11,6 +11,18 @@ function goToCriticalCare () {
   window.location.href = "critical_care_for_hospitalists.html";
 }
 
+function goToAcidBase () {
+  window.location.href = "electrolytes_acid_base.html";
+}
+
+function goToRenal () {
+  window.location.href = "renal.html";
+}
+
 function goToNeuro () {
   window.location.href = "neurology.html";
+}
+
+function goToPsych () {
+  window.location.href = "psychiatry.html";
 }
